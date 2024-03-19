ccc86d4b8db46e19ee79cb2003664d75db3ad71d
Annalisa <annalisaferlitopersonal@gmail.com>
2024-03-19 19:01:37 +0100

    commit iniziale, api

---
+++ src/components/RecipesDetails/RecipesDetails.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+
+const RecipesDetails = () => {
+
+    const [recipeDetails, setRecipeDetails] = useState([]);
+
+    
+
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default RecipesDetails
---
+++ src/components/RecipesList/RecipesList.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import "./RecipesList.css";
+
+const RecipesList = () => {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+        try {
+            const response = await fetch(
+            "https://www.themealdb.com/api/json/v1/1/search.php?s="
+            );
+            if (!response.ok) {
+            throw new Error("Errore di rete");
+            }
+            const data = await response.json();
+            setRecipes(data.meals); // Imposta le ricette nello stato
+            console.log(data); // Output dei dati ricevuti
+        } catch (errore) {
+            console.log(errore);
+        }
+    };
+
+    fetchRecipes(); // Chiama la funzione per recuperare le ricette
+  }, []); // Array vuoto, quindi l'effetto viene eseguito solo una volta al montaggio
+
+    return (
+        <div className="recipes">
+        <ul>
+            {recipes.map((recipe) => (
+            <li key={recipe.idMeal}>
+                <p>{recipe.strMeal}</p>
+                <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+            </li>
+            ))}
+        </ul>
+        </div>
+    );
+};
+
+export default RecipesList;
